ebe168390db903535a3b6cb811e6d10dfb3fd970
linjinzhen <linjinzhen@hotmail.com>
2019-03-03 20:48:18 +0800

    add "container opening range" ds version

---
+++ container opening range (ds)/code/modinfo.lua
@@ -0,0 +1,56 @@
+name = "Container Opening Range 容器开启范围"
+description = "Modify container opening range 修改容器开启范围"
+author = "linjinzhen"
+version = "1.0.5"
+forumthread = ""
+
+api_version = 6
+
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+dst_compatible = false
+
+local num_options = {}
+for i = 1, 12 do num_options[i] = {description = ""..i.."", data = i} end
+
+configuration_options = 
+{
+  {
+    name = "TREASURECHEST",
+    label = "Treasure Chest",
+    options = num_options,
+    default = 3,
+  },
+  {
+    name = "CHESTER",
+    label = "Chester",
+    options = num_options,
+    default = 3,
+  },
+  {
+    name = "ICEBOX",
+    label = "Icebox",
+    options = num_options,
+    default = 5,
+  },
+  {
+    name = "OTHER_CHEST",
+    label = "Other Chest-like Container",
+    options = num_options,
+    default = 3,
+  },
+  {
+    name = "COOKPOT",
+    label = "Cookpot",
+    options = num_options,
+    default = 3,
+  },
+  {
+    name = "OTHER_COOKER",
+    label = "Other Cooker-like Container",
+    options = num_options,
+    default = 3,
+  }
+}
+
---
+++ container opening range (ds)/code/modmain.lua
@@ -0,0 +1,34 @@
+local container_map = {}
+container_map.treasurechest = "TREASURECHEST"
+container_map.chester = "CHESTER"
+container_map.shadowchester = "CHESTER"
+container_map.hutch = "CHESTER"
+container_map.icebox = "ICEBOX"
+container_map.chest = "OTHER_CHEST"
+container_map.cookpot = "COOKPOT"
+container_map.cooker = "OTHER_COOKER"
+
+local function ModifyContainerRange(self)
+    function self:OnUpdate(dt)
+        local container_config_name = container_map[self.container.prefab]
+        if container_config_name == nil then
+            container_config_name = container_map[self.container.components.container.type]
+        end
+        local d = GetModConfigData(container_config_name)
+        if self.isopen and self.owner and self.container then
+    		if not (self.container.components.inventoryitem and self.container.components.inventoryitem:IsHeldBy(self.owner)) then
+    			local distsq = self.owner:GetDistanceSqToInst(self.container)
+    			if distsq > d*d then
+    				self:Close()
+    			end
+    		end
+    	end
+    end
+end
+
+AddClassPostConstruct("widgets/containerwidget", ModifyContainerRange)
+
+
+AddPrefabPostInit("cookpot", function(self)
+    self.components.playerprox.far = GetModConfigData(container_map.cookpot)
+end)
